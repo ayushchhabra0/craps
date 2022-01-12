44333319ce29a3ce7030c100f0d508b6b18f9b5c
Ayush Chhabra <achhabra23@students.polytechnic.org>
2022-01-11 16:50:12 -0800

    Created getResponse2() method to ask if player wants to know the instructions

---
+++ craps.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class craps {
+    public static void main(String[] args) {
+        getResponse1();
+    }
+
+    public static String getResponse1() {
+        System.out.println("Do you want to play the game of Craps? (y/n): ");
+        Scanner input = new Scanner(System.in);
+        String response1 = input.next();
+        if (response1 != "y") {
+            System.out.println("Thank you. Have a nice day!");
+        }
+        input.close();
+        return response1;
+    }
+
+    public static String getResponse2() {
+        System.out.println("Do you know how to play Craps? (y/n): ");
+        Scanner input = new Scanner(System.in);
+        String response2 = input.next();
+        input.close();
+        return response2;
+    }
+
+}
