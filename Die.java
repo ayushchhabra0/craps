492389fec4877179f5e83fde5dbdd2540032c1dc
Ayush Chhabra <achhabra23@students.polytechnic.org>
2022-01-11 16:29:40 -0800

    Created Die Roll Method with Two Die Variables

--- Die.java
+++ Die.java
@@ -1,3 +1,8 @@
 public class Die {
     
+    public int dieRoll() {
+        int roll1 = (int) Math.random() * 6 + 1;
+        int roll2 = (int) Math.random() + 6 + 1;
+        return roll1 + roll2;
+    }
 }
